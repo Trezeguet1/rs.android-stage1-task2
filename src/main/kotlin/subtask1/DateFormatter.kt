f26c905f96112d3a0a5803c2b157dfee97aa1d4f
Vladislav Zhuravel <51509139+Trezeguet1@users.noreply.github.com>
2020-04-13 15:16:58 +0300

    changes:formatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,28 @@
 package subtask1
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
+
 
 class DateFormatter {
 
     // TODO: Complete the following function
+    private val task1 = "d/M/yyyy"
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+    var day = day
+    var month = month
+    var year = year
+
+    val m = listOf(
+        "января", "февраля", "марта", "апреля", "мая", "июня",
+        "июля", "августа", "сентября", "октября", "ноября", "декабря"
+    )
+
+
+
     }
 }
